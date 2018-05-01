4a124dab42b29a14495e63faf4fe1979970adcb2
Byungwook Lee <quddnr153@gmail.com>
2018-05-01 22:59:38 +0900

    Change schema

--- src/main/resources/schema-h2.sql
+++ src/main/resources/schema-h2.sql
@@ -1,31 +1,11 @@
-CREATE TABLE IF NOT EXISTS excel_master
-(
-  excel_master_seq BIGINT AUTO_INCREMENT PRIMARY KEY
-, excel_request_user_id VARCHAR(255) NOT NULL
-, excel_request_service_id VARCHAR(255) NOT NULL
-, template_id VARCHAR(255) NOT NULL
-, request_date TIMESTAMP
-, modify_date TIMESTAMP
-, status VARCHAR(255)
-);
-
-CREATE TABLE IF NOT EXISTS excel_content
-(
-  excel_content_seq BIGINT AUTO_INCREMENT PRIMARY KEY
-, excel_master_seq BIGINT
-, physical_file_nmae VARCHAR(255)
-, logical_file_name VARCHAR(255)
-, contents VARCHAR(2048)
-, request_date TIMESTAMP
-, modify_date TIMESTAMP
-);
-
 CREATE TABLE IF NOT EXISTS excel_template
 (
-  template_id VARCHAR(255) PRIMARY KEY
-, template_file_name VARCHAR(255)
-, service_id VARCHAR(255)
-, register_date TIMESTAMP
-, modify_date TIMESTAMP
-, modifier_id VARCHAR(255)
+  exel_tempate_seq BIGINT AUTO_INCREMENT PRIMARY KEY
+, file_path VARCHAR(255)
+, file_name VARCHAR(255)
+, service_code VARCHAR(255)
+, description VARCHAR(255)
+, register_date DATE
+, modify_date DATE
+, model_json_format VARCHAR(2048)
 );
